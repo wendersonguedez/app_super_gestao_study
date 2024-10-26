9b21ed6d9017edf75b262d0892734d2fcfd1e6be
wenderson <wendersonguedes6@gmail.com>
2024-10-26 15:14:00 -0300

    refactor: removendo lixo

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,7 +8,6 @@ class ContatoController extends Controller
 {
     public function index()
     {
-        var_dump($_POST);
         return view('site.contato', ['title' => 'Contatoooo']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [PrincipalController::class, 'index'])->name('principal.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('sobre-nos.index');
 Route::get('/contato', [ContatoController::class, 'index'])->name('contato.index');
